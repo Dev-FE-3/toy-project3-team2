48877460802695729fde25bee865430e87583c35
Jang Eunhye <hh961130@naver.com>
2025-03-31 19:46:47 +0900

    fix: 경로 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import reactLogo from './assets/imgs/react.svg'
+import viteLogo from './assets/imgs/vite.svg'
 import './styles/App.css'
 
 function App() {
