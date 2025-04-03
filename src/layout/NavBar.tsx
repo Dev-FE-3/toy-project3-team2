5399eabffd14053f5ac1f6db98a0963fba0ff1ae
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-04-03 14:01:07 +0900

    Design: Nav Bar Active 스타일 추가

--- src/layout/NavBar.tsx
+++ src/layout/NavBar.tsx
@@ -1,28 +1,46 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 import Home from "../assets/icons/home.svg";
+import FillHome from "../assets/icons/fill-home.svg";
 import BookMark from "../assets/icons/bookmark.svg";
+import FillBookMark from "../assets/icons/fill-bookmark.svg";
 import Add from "../assets/icons/add.svg";
 import User from "../assets/icons/user.svg";
+import FillUser from "../assets/icons/fill-user.svg";
 
 const menuItems = [
-  { path: "/", icon: Home, label: "홈" },
-  { path: "/subscriptions", icon: BookMark, label: "구독" },
-  { path: "/playlist/create", icon: Add, label: "생성" },
-  { path: "/mypage", icon: User, label: "마이페이지" },
+  { path: "/", icon: [Home, FillHome], label: "홈" },
+  { path: "/subscriptions", icon: [BookMark, FillBookMark], label: "구독" },
+  { path: "/playlist/create", icon: [Add, ""], label: "생성" },
+  { path: "/mypage", icon: [User, FillUser], label: "마이페이지" },
 ];
 
-function NavBar() {
+const NavBar = () => {
+  const location = useLocation(); // 현재 URL 가져오기
+
   return (
-    <nav className="fixed bottom-0 flex w-full max-w-[430px] h-[58px] justify-around items-center bg-[#202525] rounded-t-[10px]">
-      {menuItems.map(({ path, icon, label }) => (
-        <Link key={path} to={path} className="flex flex-col items-center gap-1">
-          <img src={icon} alt={label} />
-          <p className="text-center text-white text-[10px]">{label}</p>
-        </Link>
-      ))}
+    <nav className="fixed bottom-0 flex w-full max-w-[430px] h-[58px] justify-around items-center bg-background-container rounded-t-[10px]">
+      {menuItems.map(({ path, icon, label }) => {
+        const isActive = location.pathname === path; // 현재 URL과 메뉴 path 비교
+
+        return (
+          <Link
+            key={path}
+            to={path}
+            className={`flex flex-col items-center justify-center gap-1 ${
+              isActive ? "text-main text-tab-bold" : "text-primary"
+            }`}
+          >
+            <div className="w-[20px] flex justify-center items-center">
+              {isActive ? <img src={icon[1]} alt={label} /> : <img src={icon[0]} alt={label} />}
+            </div>
+
+            <p className="text-center text-tab">{label}</p>
+          </Link>
+        );
+      })}
     </nav>
   );
-}
+};
 
 export default NavBar;
