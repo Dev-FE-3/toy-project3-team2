0c7354d4b9e3c396658d915c3db86d934b137bdf
ijisun <ijisun9292@gmail.com>
2025-04-04 00:05:59 +0900

    refactor: 시멘틱한 태그 적용 및 불필요한 div 제거

--- src/components/common/PlaylistCard.tsx
+++ src/components/common/PlaylistCard.tsx
@@ -16,36 +16,33 @@ const PlaylistCard: React.FC<PlaylistCardProps> = ({
   isOwner,
 }) => {
   return (
-    <div>
+    <>
       {/* 썸네일 */}
-      <div className="w-full">
-        <img src={thumbnailUrl} alt="Playlist Thumbnail" className="w-full object-cover" />
-      </div>
+      <img src={thumbnailUrl} alt="Playlist Thumbnail" className="w-full object-cover" />
 
       {/* 정보 영역 */}
-      <div className="px-[16px] py-[12px] flex flex-row gap-[8px]">
+      <div className="px-[16px] py-[12px] flex flex-row">
         {/* 유저 프로필 이미지 (타인의 플레이리스트만 표시) */}
         {!isOwner && userImage && (
-          <img src={userImage} alt="User" className="w-[36px] h-[36px] rounded-full" />
+          <img src={userImage} alt="User" className="w-[36px] h-[36px] rounded-full mr-[8px]" />
         )}
 
-        <div>
+        <div className="flex-1">
           {/* 플레이리스트 제목 */}
-          <div className="flex flex-row gap-[2px]">
-            <div>
-              {/* 비공개 아이콘 */}
-              {!isPublic && (
-                <img
-                  src="/src/assets/icons/icon-lock.svg"
-                  className="w-[16px] h-[16px] inline mr-[4px]"
-                />
-              )}
-              {/* 제목 */}
-              <span className="text-font-primary text-body2">{title}</span>
-            </div>
+          <div className="flex justify-between items-start">
+            {/* 비공개 아이콘 */}
+            {!isPublic && (
+              <img
+                src="/src/assets/icons/icon-lock.svg"
+                className="w-[16px] h-[16px] inline mr-[4px] mt-[2px]"
+              />
+            )}
+            {/* 제목 */}
+            <h3 className="text-font-primary text-body2 leading-[1.5]">{title}</h3>
+
             {/* 수정, 삭제 메뉴 (본인의 플레이리스트일 때만 표시) */}
             {isOwner && (
-              <div role="button" className="mt-[5px]">
+              <div role="button" className="mt-[2px]">
                 <img
                   src="src/assets/icons/icon-menu-dots-vertical.svg"
                   alt="메뉴"
@@ -54,13 +51,14 @@ const PlaylistCard: React.FC<PlaylistCardProps> = ({
               </div>
             )}
           </div>
+
           {/* 좋아요, 구독, 댓글 */}
           <div className="mt-[12px] mb-[6px]">
             <PlaylistActions playlistId="text-id" />
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
