17a3623bc4c8600f7ee8359d7a8ac11f9dd66f9e
임하림 <gkfla668@naver.com>
2025-04-04 11:09:40 +0900

    Feat: 라우팅 및 공통 레이아웃 (#1)
    * Design: 기본 레이아웃 스타일 설정
    
    * Chore: react-router-dom 패키지 설치
    
    * Feat: 라우팅 설정 및 Navbar 추가
    
    * Design: 사용하지 않는 css 파일 삭제 및 수정
    
    * Design: Nav Bar Active 스타일 추가
    
    * Feat: header 추가 및 레이아웃 수정
    
    * Design: 레이아웃 수정
    
    * Feat: Logo 클릭 시 home '/' 페이지로 이동
    
    * Refactor: 타이틀 h1 태그로 변경
    
    * Design: Nav Bar 메뉴 영역 확장
    
    * Fix: 메뉴명 수정
    
    * Refactor: 배경색 tailwind 설정 값으로 변경

---
+++ src/pages/EditProfile.tsx
@@ -0,0 +1,7 @@
+/** 사용자 정보를 수정하는 페이지 */
+
+const EditProfile = () => {
+  return <div>사용자 정보를 수정하는 페이지입니다.</div>;
+};
+
+export default EditProfile;
---
+++ src/pages/PlaylistDetail.tsx
@@ -0,0 +1,7 @@
+/** 플레이리스트 상세 페이지 */
+
+const PlaylistDetail = () => {
+  return <div>플레이리스트 상세 페이지입니다.</div>;
+};
+
+export default PlaylistDetail;
