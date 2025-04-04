fd5db56154ca2aa642330dd35cc8d175c98da621
Jang Eunhye <hh961130@naver.com>
2025-04-04 10:26:51 +0900

    Chore: guide파일 수정(기능별 분리)

--- src/components/common/guide.tsx
+++ src/components/common/guide.tsx
@@ -1,25 +1,55 @@
+import { useState } from "react";
 import { Button } from "./button";
+import { Input } from "./input";
+
+const ButtonGuide = () => {
+  return (
+    <div>
+      <Button variant="full">저장</Button>
+      <br />
+      <Button disabled variant="full">
+        저장
+      </Button>
+      <br />
+      <Button variant="small">중복확인</Button>
+      <br />
+      <Button disabled variant="small">
+        중복확인
+      </Button>
+      <br />
+      <Button variant="secondary">닫기</Button>
+      <br />
+    </div>
+  );
+};
+
+const InputGuide = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  return (
+    <div>
+      <Input type="text" placeholder="이름을 입력하세요" />
+      <Input type="password" placeholder="비밀번호를 입력하세요" />
+      {/* delete 버튼 옵션 추가 */}
+      <Input type="email" showDelete placeholder="이메일을 입력하세요" />
+      <Input
+        placeholder="텍스트를 입력하세요"
+        value={inputValue}
+        defaultValue="defaultValue"
+        onChange={(e) => setInputValue(e.target.value)}
+        showDelete
+      />
+      {/* inputValue 확인 */}
+      <p>{inputValue}</p>
+    </div>
+  );
+};
 
 const Guide = () => {
   return (
     <>
-      {/* Button Guide */}
-      <div>
-        <Button variant="full">저장</Button>
-        <br />
-        <Button disabled variant="full">
-          저장
-        </Button>
-        <br />
-        <Button variant="small">중복확인</Button>
-        <br />
-        <Button disabled variant="small">
-          중복확인
-        </Button>
-        <br />
-        <Button variant="secondary">닫기</Button>
-        <br />
-      </div>
+      <ButtonGuide />
+      <InputGuide />
     </>
   );
 };
