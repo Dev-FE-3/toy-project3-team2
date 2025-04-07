bf40ed7b7acf0f39f92a544fc23bd83b0efec070
Jang Eunhye <hh961130@naver.com>
2025-04-07 22:26:55 +0900

    Design: placeholder문구 수정 (login)

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,26 +4,32 @@ import { Input } from "../components/common/Input";
 
 const Login = () => {
   return (
-    <div className="flex h-screen flex-col justify-center">
-      <div className="mb-[60px] flex justify-center">
-        <img src={Logo} alt="logo" className="h-[35px]" />
-      </div>
+    <div className="px-4">
+      <div className="flex h-screen flex-col justify-center">
+        <div className="mb-[60px] flex justify-center">
+          <img src={Logo} alt="logo" className="h-[35px]" />
+        </div>
 
-      {/* 이메일 / 비밀번호 */}
-      <p className="mb-2 text-sub2">이메일</p>
-      <Input type="email" placeholder="이메일을 입력하세요" className="mb-5" />
-      <p className="mb-2 text-sub2">비밀번호</p>
-      <Input type="password" placeholder="비밀번호를 입력하세요" className="mb-6" />
+        {/* 이메일 / 비밀번호 */}
+        <div>
+          <p className="mb-2 text-sub2">이메일</p>
+          <Input type="email" placeholder="이메일을 입력해주세요" className="mb-5" />
+        </div>
+        <div>
+          <p className="mb-2 text-sub2">비밀번호</p>
+          <Input type="password" placeholder="비밀번호를 입력해주세요" className="mb-6" />
+        </div>
 
-      {/* 로그인 버튼 */}
-      <Button variant="full" className="mb-10">
-        로그인
-      </Button>
+        {/* 로그인 버튼 */}
+        <Button variant="full" className="mb-10">
+          로그인
+        </Button>
 
-      {/* 회원가입하러 가기 */}
-      <div className="text-center">
-        <span className="mr-2 text-sub text-font-muted">아직 회원이 아니신가요?</span>
-        <a className="cursor-pointer text-sub-bold text-font-primary hover:underline">회원가입</a>
+        {/* 회원가입하러 가기 */}
+        <div className="text-center">
+          <span className="mr-2 text-sub text-font-muted">아직 회원이 아니신가요?</span>
+          <a className="cursor-pointer text-sub-bold text-font-primary hover:underline">회원가입</a>
+        </div>
       </div>
     </div>
   );
