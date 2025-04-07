cb64592e66e865661e6f8ca66ad89a20b782fe1a
Jang Eunhye <hh961130@naver.com>
2025-04-07 14:47:37 +0900

    Design: 로그인페이지 퍼블리싱

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,17 +1,30 @@
 import Logo from "../assets/imgs/logo.svg";
+import { Button } from "../components/common/Button";
+import { Input } from "../components/common/Input";
 
 const Login = () => {
   return (
-      <div>
-        <h1>어쩌고</h1>
-
-        <img src={Logo} alt="logo" className="my-4h-[35px] flex justify-center" />
-        {/* 이메일 / 비밀번호 */}
-
-        {/* 로그인 버튼 */}
-
-        {/* 회원가입하러 가기 */}
+    <div className="flex h-screen flex-col justify-center">
+      <div className="mb-[60px] flex justify-center">
+        <img src={Logo} alt="logo" className="h-[35px]" />
       </div>
+
+      {/* 이메일 / 비밀번호 */}
+      <p className="mb-2">이메일</p>
+      <Input type="email" placeholder="이메일을 입력하세요" className="mb-5" />
+      <p className="mb-2">비밀번호</p>
+      <Input type="password" placeholder="비밀번호를 입력하세요" className="mb-5" />
+
+      {/* 로그인 버튼 */}
+      <Button variant="full" className="mb-10">
+        로그인
+      </Button>
+
+      {/* 회원가입하러 가기 */}
+      <p className="text-center">
+        아직 회원이 아니신가요? <a>회원가입</a>
+      </p>
+    </div>
   );
 };
 
