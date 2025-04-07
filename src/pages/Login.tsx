6f1d03951bacaf85d1d31ccb9eca9ba37f087938
Jang Eunhye <hh961130@naver.com>
2025-04-07 13:28:33 +0900

    Design: 주석

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,18 @@
+import Logo from "../assets/imgs/logo.svg";
+
 const Login = () => {
-  return <div>로그인 페이지입니다.</div>;
+  return (
+      <div>
+        <h1>어쩌고</h1>
+
+        <img src={Logo} alt="logo" className="my-4h-[35px] flex justify-center" />
+        {/* 이메일 / 비밀번호 */}
+
+        {/* 로그인 버튼 */}
+
+        {/* 회원가입하러 가기 */}
+      </div>
+  );
 };
 
 export default Login;
