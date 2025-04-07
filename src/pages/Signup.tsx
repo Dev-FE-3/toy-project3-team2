8a99d48f489db95f6f48e55f768933715e64a032
Jang Eunhye <hh961130@naver.com>
2025-04-07 22:29:03 +0900

    Style: 중복div 제거

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -9,54 +9,52 @@ const Signup = () => {
   const [isNicknameValid, setIsNicknameValid] = useState<boolean | null>(true);
 
   return (
-    <div className="px-4">
-      <div className="mt-6 space-y-5">
-        <div>
-          <p className="mb-2 text-sub2">이메일*</p>
-          <div className="flex gap-2">
-            <Input type="email" placeholder="이메일을 입력해주세요" className="flex-grow" />
-            <Button>중복확인</Button>
-          </div>
-          {isEmailValid === false && (
-            <p className="mt-2 text-sub text-red-500">
-              <img src={errorIcon} alt="error" className="mr-1 inline-block h-4 w-4" />
-              가입된 이메일입니다.
-            </p>
-          )}
-          {isEmailValid === true && (
-            <p className="mt-2 text-sub text-green-500">
-              <img src={successIcon} alt="success" className="mr-1 inline-block h-4 w-4" />
-              사용 가능한 이메일입니다.
-            </p>
-          )}
+    <div className="mt-6 space-y-5 px-4">
+      <div>
+        <p className="mb-2 text-sub2">이메일*</p>
+        <div className="flex gap-2">
+          <Input type="email" placeholder="이메일을 입력해주세요" className="flex-grow" />
+          <Button>중복확인</Button>
         </div>
-        <div>
-          <p className="mb-2 text-sub2">닉네임*</p>
-          <div className="flex gap-2">
-            <Input type="text" placeholder="닉네임을 입력해주세요" className="flex-grow" />
-            <Button>중복확인</Button>
-          </div>
-          {isNicknameValid === false && (
-            <p className="mt-2 text-sub text-red-500">
-              <img src={errorIcon} alt="error" className="mr-1 inline-block h-4 w-4" />
-              사용 중인 닉네임입니다.
-            </p>
-          )}
-          {isNicknameValid === true && (
-            <p className="mt-2 text-sub text-green-500">
-              <img src={successIcon} alt="success" className="mr-1 inline-block h-4 w-4" />
-              사용 가능한 닉네임입니다.
-            </p>
-          )}
+        {isEmailValid === false && (
+          <p className="mt-2 text-sub text-red-500">
+            <img src={errorIcon} alt="error" className="mr-1 inline-block h-4 w-4" />
+            가입된 이메일입니다.
+          </p>
+        )}
+        {isEmailValid === true && (
+          <p className="mt-2 text-sub text-green-500">
+            <img src={successIcon} alt="success" className="mr-1 inline-block h-4 w-4" />
+            사용 가능한 이메일입니다.
+          </p>
+        )}
+      </div>
+      <div>
+        <p className="mb-2 text-sub2">닉네임*</p>
+        <div className="flex gap-2">
+          <Input type="text" placeholder="닉네임을 입력해주세요" className="flex-grow" />
+          <Button>중복확인</Button>
         </div>
-        <div>
-          <p className="mb-2 text-sub2">비밀번호*</p>
-          <Input type="password" placeholder="비밀번호를 입력해주세요" />
-        </div>
-        <div>
-          <p className="mb-2 text-sub2">비밀번호 확인*</p>
-          <Input type="password" placeholder="비밀번호를 다시 입력해주세요" />
-        </div>
+        {isNicknameValid === false && (
+          <p className="mt-2 text-sub text-red-500">
+            <img src={errorIcon} alt="error" className="mr-1 inline-block h-4 w-4" />
+            사용 중인 닉네임입니다.
+          </p>
+        )}
+        {isNicknameValid === true && (
+          <p className="mt-2 text-sub text-green-500">
+            <img src={successIcon} alt="success" className="mr-1 inline-block h-4 w-4" />
+            사용 가능한 닉네임입니다.
+          </p>
+        )}
+      </div>
+      <div>
+        <p className="mb-2 text-sub2">비밀번호*</p>
+        <Input type="password" placeholder="비밀번호를 입력해주세요" />
+      </div>
+      <div>
+        <p className="mb-2 text-sub2">비밀번호 확인*</p>
+        <Input type="password" placeholder="비밀번호를 다시 입력해주세요" />
       </div>
     </div>
   );
