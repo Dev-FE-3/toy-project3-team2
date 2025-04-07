dc03408ab5f6593d8cb14c2c79cb88811288e2a7
nani <pha3113@naver.com>
2025-04-07 12:16:04 +0900

    Feat : OverflowMenu guide에 추가 및 myPage에서 제거

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,16 +1,6 @@
-import OverflowMenu from "../components/common/OverflowMenu";
-
-const menuOptions = [
-  { label: "수정", action: () => alert("수정 클릭") },
-  { label: "삭제", action: () => alert("삭제 클릭") }
-];
-
 const MyPage = () => {
   return (
-    <div>
-      마이페이지 입니다.
-      <OverflowMenu options={menuOptions} />
-    </div>
+    <div>마이페이지 입니다.</div>
   )
 };
 
