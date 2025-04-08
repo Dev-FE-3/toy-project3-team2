d1ab1162de1872a3f914d19883a58c4f235e2f92
nani <pha3113@naver.com>
2025-04-08 20:05:11 +0900

    Feat: 마이페이지 퍼블리싱

---
+++ src/components/DropDownMenu.tsx
@@ -0,0 +1,57 @@
+import { useState, useRef } from "react";
+import IconArrowBottom from "../assets/icons/arrow-bottom.svg?react";
+
+const MENU_OPTIONS = ["최신 등록순", "오래된 등록순", "업데이트순", "구독순", "좋아요순", "댓글순"];
+
+const DropDownMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState(MENU_OPTIONS[0]);
+  const dropdownRef = useRef<HTMLButtonElement>(null);
+
+  const toggleDropDown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+  const selectOption = (option: string) => {
+    setSelected(option);
+    setIsOpen(false);
+  };
+
+  return (
+    <>
+      <button
+        ref={dropdownRef}
+        className="inline-flex items-center justify-between text-body2"
+        onClick={toggleDropDown}
+        aria-haspopup="true"
+        aria-expanded={isOpen}
+        aria-controls="dropdown-menu"
+      >
+        {selected}
+        <IconArrowBottom className="ml-[4px]" />
+      </button>
+
+      <ul
+        id="dropdown-menu"
+        role="menu"
+        className={`fixed bottom-0 left-0 right-0 z-50 mx-auto max-w-[430px] overflow-hidden rounded-t-[10px] bg-background-container transition-transform duration-300 ease-in-out ${isOpen ? "pointer-events-auto translate-y-0" : "pointer-events-none translate-y-full"}`}
+        style={{ willChange: "transform" }}
+      >
+        {MENU_OPTIONS.map((option, index) => (
+          <li key={option} role="menuitem" className="px-[10px] hover:bg-outline">
+            <button
+              className={`w-full py-[17px] text-body2 ${
+                index !== MENU_OPTIONS.length - 1 ? "border-b border-outline" : ""
+              }`}
+              onClick={() => selectOption(option)}
+            >
+              {option}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default DropDownMenu;
--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,7 +1,59 @@
+import PlaylistCard from "../components/common/PlaylistCard";
+import DropDownMenu from "../components/DropDownMenu";
+
 const MyPage = () => {
   return (
-    <div>마이페이지 입니다.</div>
-  )
+    <>
+      {/* user 정보 */}
+      <div className="flex flex-wrap items-center gap-[14px] p-[16px]">
+        <img
+          src="https://i.pinimg.com/736x/60/0c/b6/600cb65bd5f67e70a8fac0909e4c1ee6.jpg"
+          alt="User Profile"
+          className="h-[60px] w-[60px] rounded-full object-cover"
+        />
+        <div className="flex w-[calc(100%-74px)] flex-col gap-[4px]">
+          <span>칠걸스</span>
+          <span className="text-sub text-font-muted">구독 365</span>
+        </div>
+        <p className="my-[2px] w-full text-font-primary">소개글을 작성해주세요</p>
+      </div>
+
+      {/* user가 생성한 플레이리스트 */}
+      <div className="border-t border-outline">
+        <div className="px-[20px] py-[12px] text-right">
+          <DropDownMenu />
+        </div>
+        <ul>
+          <li>
+            <PlaylistCard
+              id="dummyId-001"
+              title="[Ghibli OST Playlist] 감성 충만 지브리 OST 연주곡 모음집"
+              thumbnailUrl="https://i.pinimg.com/736x/60/0c/b6/600cb65bd5f67e70a8fac0909e4c1ee6.jpg"
+              isOwner={true}
+            />
+          </li>
+          <li>
+            <PlaylistCard
+              id="dummyId-002"
+              title="[Ghibli OST Playlist] 감성 충만 지브리 OST 연주곡 모음집"
+              thumbnailUrl="https://i.pinimg.com/736x/60/0c/b6/600cb65bd5f67e70a8fac0909e4c1ee6.jpg"
+              isPublic={false}
+              isOwner={true}
+            />
+          </li>
+          <li>
+            <PlaylistCard
+              id="dummyId-003"
+              title="[Ghibli OST Playlist] 감성 충만 지브리 OST 연주곡 모음집"
+              thumbnailUrl="https://i.pinimg.com/736x/60/0c/b6/600cb65bd5f67e70a8fac0909e4c1ee6.jpg"
+              isPublic={false}
+              isOwner={true}
+            />
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default MyPage;
