1785eabb8537de91c712658e736fbb3237aa30dc
Jang Eunhye <hh961130@naver.com>
2025-04-08 13:42:47 +0900

    Style: 시맨틱 태그로 수정

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,11 @@ import { Button } from "../components/common/Button";
 import { Input } from "../components/common/Input";
 
 const Login = () => {
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    // 로그인 로직 구현
+  };
+
   return (
     <div className="px-4">
       <div className="flex h-screen flex-col justify-center">
@@ -10,22 +15,37 @@ const Login = () => {
           <img src={Logo} alt="logo" className="h-[35px]" />
         </div>
 
-        {/* 이메일 / 비밀번호 */}
-        <div>
-          <p className="mb-2 text-sub2">이메일</p>
-          <Input type="email" placeholder="이메일을 입력해주세요" className="mb-5" />
-        </div>
-        <div>
-          <p className="mb-2 text-sub2">비밀번호</p>
-          <Input type="password" placeholder="비밀번호를 입력해주세요" className="mb-6" />
-        </div>
+        <form onSubmit={handleSubmit} className="space-y-5">
+          <div>
+            <label htmlFor="email" className="mb-2 text-sub2">
+              이메일
+            </label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="이메일을 입력해주세요"
+              className="mb-5"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="mb-2 text-sub2">
+              비밀번호
+            </label>
+            <Input
+              id="password"
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              className="mb-6"
+              required
+            />
+          </div>
 
-        {/* 로그인 버튼 */}
-        <Button variant="full" className="mb-10">
-          로그인
-        </Button>
+          <Button type="submit" variant="full" className="mb-10">
+            로그인
+          </Button>
+        </form>
 
-        {/* 회원가입하러 가기 */}
         <div className="text-center">
           <span className="mr-2 text-sub text-font-muted">아직 회원이 아니신가요?</span>
           <a className="cursor-pointer text-sub-bold text-font-primary hover:underline">회원가입</a>
