653c66ec1357cf3c5984d130c3cca783917509ce
ijisun <ijisun9292@gmail.com>
2025-04-10 18:07:48 +0900

    Refactor: react-query 설정을 main.tsx에서 App.tsx로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState, useEffect } from "react";
 // import axiosInstance from "./services/axios/axiosInstance";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
@@ -19,6 +20,9 @@ import Guide from "./pages/Guide";
 //   title: string;
 //   info: string;
 // }
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 function App() {
   // const [items, setItems] = useState<PlaylistItem[]>([]);
@@ -41,33 +45,35 @@ function App() {
   // }, []);
 
   return (
-    <Router>
-      <Routes>
-        {/* 헤더 & 네비게이션 X */}
-        <Route path="/login" element={<Login />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          {/* 헤더 & 네비게이션 X */}
+          <Route path="/login" element={<Login />} />
 
-        {/* Layout이 적용된 페이지들 */}
-        <Route element={<Layout />}>
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/user/edit" element={<EditProfile />} />
-          <Route path="/playlist/create" element={<PlaylistCreate />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/mypage" element={<MyPage />} />
-          <Route path="/subscriptions" element={<Subscriptions />} />
-          <Route path="/playlist/:id" element={<PlaylistDetail />} />
-          <Route path="/guide" element={<Guide />} />
-        </Route>
-      </Routes>
-      {/* <h1>플레이리스트</h1>
-      <ul>
-        {items.map((item) => (
-          <li key={item.id}>
-            <h2>제목: {item.title}</h2>
-            <p>소개글: {item.info}</p>
-          </li>
-        ))}
-      </ul> */}
-    </Router>
+          {/* Layout이 적용된 페이지들 */}
+          <Route element={<Layout />}>
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/user/edit" element={<EditProfile />} />
+            <Route path="/playlist/create" element={<PlaylistCreate />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/mypage" element={<MyPage />} />
+            <Route path="/subscriptions" element={<Subscriptions />} />
+            <Route path="/playlist/:id" element={<PlaylistDetail />} />
+            <Route path="/guide" element={<Guide />} />
+          </Route>
+        </Routes>
+        {/* <h1>플레이리스트</h1>
+        <ul>
+          {items.map((item) => (
+            <li key={item.id}>
+              <h2>제목: {item.title}</h2>
+              <p>소개글: {item.info}</p>
+            </li>
+          ))}
+        </ul> */}
+      </Router>
+    </QueryClientProvider>
   );
 }
 
