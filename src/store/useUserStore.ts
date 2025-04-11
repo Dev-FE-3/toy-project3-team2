943a692bae59457b506c0255483f373d355e7e4d
nani <pha3113@naver.com>
2025-04-11 20:49:34 +0900

    Feat: 유저 정보 zustand 미들웨어로 로컬스토리지에 저장

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 import { User } from "../types/user";
 
 interface UserState {
@@ -7,8 +8,15 @@ interface UserState {
   clearUser: () => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
-  user: null,
-  setUser: (user) => set({ user }),
-  clearUser: () => set({ user: null }),
-}));
+export const useUserStore = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      setUser: (user) => set({ user }),
+      clearUser: () => set({ user: null }),
+    }),
+    {
+      name: "user-storage",
+    },
+  ),
+);
