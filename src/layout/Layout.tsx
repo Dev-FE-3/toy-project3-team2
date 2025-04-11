b339a89ece87334f8a080be4830dd85c90543a7b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2025-04-11 13:43:06 +0900

    Design: Outlet scroll 처리

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -9,14 +9,13 @@ const Layout = () => {
   const hideAll = location.pathname === "/login";
   const hideNavOnly = ["/signup", "/user/edit", "/playlist/create"].includes(location.pathname);
 
-  // h-screen을 사용하면 콘텐츠가 길어질 때 배경이 흰색으로 깨지는 이슈 발생
-  // min-h-screen으로 수정하여 콘텐츠 길이에 따라 유동적으로 높이 조절되도록 함
-  // 네브바는 fixed이므로, 아래 콘텐츠가 가려지지 않도록 pb-[58px] 추가
-
   return (
     <div className="flex min-h-screen flex-col">
       {!hideAll && <Header />}
-      <div className="pb-[73px] pt-[60px]">
+      <div
+        className="scrollbar-hide overflow-y-auto pt-[60px]"
+        style={{ height: "calc(100vh - 60px)" }}
+      >
         <Outlet />
       </div>
 
