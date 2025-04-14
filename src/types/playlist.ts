fce3e8f3082e05934ea4b4d44dcaf4d9f3e9d5cf
ijisun <ijisun9292@gmail.com>
2025-04-14 20:24:09 +0900

    Fix: user id 패칭을 위한 타입 및 API 응답 구조 수정

--- src/types/playlist.ts
+++ src/types/playlist.ts
@@ -19,6 +19,7 @@ export interface Playlist {
 export interface PlaylistDetailData extends Playlist {
   videos: Video[];
   user?: {
+    id: string;
     nickname: string;
     profile_image: string;
   };
