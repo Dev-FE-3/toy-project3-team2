0a4a911a794b70fa6464c446773ee2c48bc6d74e
Jang-eunhye <hh961130@naver.com>
2025-04-14 11:33:46 +0900

    Feat: 로그인 회원가입 추가 기능 구현 (#72)
    * Design: 주석
    
    * Design: 로그인페이지 퍼블리싱
    
    * Design: 로그인 페이지 디자인 추가 수정
    
    * Design: placeholder문구 수정 (login)
    
    * Design: 회원가입 페이지 퍼블리싱
    
    * Style: 중복div 제거
    
    * Style: 시맨틱 태그로 수정
    
    * Feat: 회원가입 페이지로 이동 로직 추가
    
    * Feat: supabase 연동
    
    * Feat: 닉네임 추가 저장
    
    * Feat: 로그인 후 유저 정보 저장 로직 추가, 주석
    
    * Fix: input버튼 수정, 회원가입 링크 마진 추가
    
    * Fix: 태그 위치 오류 수정, input props수정
    
    * Feat: 회원가입 유효성 검사, 알럿창
    
    * Feat: 값에 따른 버튼 활성화/비활성화 기능
    
    * Feat: 로그인/회원가입 에러 처리 로직 개선
    
    * Feat: 로그인 시 유저 정보 Zustand store에 저장
    
    * Feat: Protected Route 구현, login페이지로 리다이렉트(임시 주석처리)
    
    * Feat: 비밀번호 유효성 검사
    
    * feat: 로그인 페이지 접근 시 세션 확인 및 리다이렉트 처리
    
    * Feat: 회원가입 후 로그아웃 처리 임시 주석
    
    * Feat: 회원가입 후 로그아웃 처리
    
    * Rename: ProtectedRoute 폴더위치 이동(components-> routes)

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,12 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { useUserStore } from "../store/useUserStore";
+
+export const ProtectedRoute = () => {
+  const user = useUserStore((state) => state.user);
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+};
