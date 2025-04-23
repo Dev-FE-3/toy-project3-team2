5d1fa6e7de34a17f682f303442959df50dbcfc57
Jang-eunhye <hh961130@naver.com>
2025-04-23 14:17:15 +0900

    Refactor: 코드리뷰 반영, 추가 리팩토링 (#185)
    * Refactor: 불필요한 상태 제거
    
    * Refactor: inputElement 함수 방식으로 변경
    
    * Refactor: SearchBar 불필요한 코드 제거
    
    * Refactor: react-hook-form 적용, ValidationMessage 컴포넌트 분리
    
    * Refactor: login페이지 react-hook-form으로 통일
    
    * Refactor: Guide 추가(ValidationMessage컴포넌트)
    
    * Fix: 스크롤 오류 해결
    
    * Refactor: 불필요한 훅,로직,데이터 제거
    
    * Refactor: 에러코드 상수파일로 분리
    
    * Refactor: usePlaylistsQuery분리
    
    * Refactor: 플레이리스트 목록 컴포넌트로 분리, type 분리
    
    * Refactor: 목록컴포넌트 구독페이지에 적용
    
    * Refactor: usePlaylistQuery수정 및 적용
    
    * Refactor: useInfiniteScrollHandler 분리 및 적용
    
    * Rename: 파일 위치 수정
    
    * Refactor: 리스트페이지(홈,구독) 스켈레톤UI 적용
    
    * Fix: 로그인 로직 수정
    
    * Fix: password value 수정
    
    * Refactor: form autoComplete속성 추가
    
    * Refactor: usePlaylists -> usePlaylistsQuery 훅으로 통합
    
    * Refactor: hasmore변수 제거
    
    * REfactor: uselayouyEffect-> useEffect로 변경
    
    * Refactor: 불필요한 div제거
    
    * Refactor: 유효성검사 validation파일 사용

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,20 +1,31 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useForm } from "react-hook-form";
 import { Link, useNavigate } from "react-router-dom";
 
-import ErrorIcon from "@/assets/icons/error.svg";
 import Logo from "@/assets/imgs/logo.svg";
 import { Button } from "@/components/common/Button";
 import { Input } from "@/components/common/Input";
+import { ValidationMessage } from "@/components/common/ValidationMessage";
 import supabase from "@/services/supabase/supabaseClient";
 import useUserStore from "@/store/useUserStore";
 import { showToast } from "@/utils/toast";
 
+interface LoginForm {
+  email: string;
+  password: string;
+}
+
 const Login = () => {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
   const setUser = useUserStore((state) => state.setUser);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    watch,
+  } = useForm<LoginForm>();
 
   useEffect(() => {
     const checkSession = async () => {
@@ -28,15 +39,15 @@ const Login = () => {
     checkSession();
   }, [navigate]);
 
+  const email = watch("email");
+  const password = watch("password");
+
   // 로그인 버튼 활성화 상태 체크
-  const isLoginEnabled = email.trim() !== "" && password.trim() !== "";
+  const isLoginEnabled = email?.trim() !== "" && password?.trim() !== "";
 
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setError("");
-
+  const onSubmit = async (_: LoginForm) => {
     try {
-      const { data } = await supabase.auth.signInWithPassword({
+      await supabase.auth.signInWithPassword({
         email,
         password,
       });
@@ -65,7 +76,10 @@ const Login = () => {
       showToast("success", `${userData.nickname}님, 환영합니다`);
     } catch (error) {
       console.error("로그인 에러:", error);
-      setError("이메일과 비밀번호를 확인해주세요.");
+      setError("root", {
+        type: "manual",
+        message: "이메일과 비밀번호를 확인해주세요.",
+      });
     }
   };
 
@@ -76,35 +90,37 @@ const Login = () => {
           <img src={Logo} alt="logo" className="h-[35px]" />
         </div>
 
-        <form onSubmit={handleSubmit} className="space-y-5">
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-5">
           <Input
             data-testid="email-input"
             htmlFor="email"
             type="text"
+            autoComplete="email"
             placeholder="이메일을 입력해주세요"
             className="mb-5"
             required
             label="이메일"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            {...register("email")}
           />
           <div>
             <Input
               data-testid="password-input"
               htmlFor="password"
               type="password"
+              autoComplete="current-password"
               placeholder="비밀번호를 입력해주세요"
               className="mb-6"
               required
               label="비밀번호"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              value={password || ""}
+              {...register("password")}
             />
-            {error && (
-              <p className="mt-[-16px] text-sub text-red-500">
-                <img src={ErrorIcon} alt="error" className="mr-1 inline-block h-4 w-4" />
-                {error}
-              </p>
+            {errors.root && (
+              <ValidationMessage
+                className="mt-[-16px]"
+                type="error"
+                message={errors.root.message || ""}
+              />
             )}
           </div>
           <Button
